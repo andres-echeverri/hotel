0c3bb291a32888a90b66acba69575c8484697c38
Andres Echeverry <andres.echeverry@vivaair.com>
2023-03-10 19:16:09 -0500

    remove link in the interface

--- src/app/core/interfaces/general-info.ts
+++ src/app/core/interfaces/general-info.ts
@@ -1,5 +1,5 @@
 export interface cardHotel {
-    image: "https://images.prismic.io/vivaair-cms/530da708-3d56-4ccf-ac79-a3937ad0e8de_barrio-san-blas.png?auto=compress,format",
+    image: string,
     hotel: string,
     descripcion: string,
     ubicacion: string,
